71acdc27d5aad85776be32581ffa97af2f6a2ced
matischlegel <161637922+matischlegel@users.noreply.github.com>
2024-07-12 15:44:44 +0000

    agregue las rutas

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,8 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +22,59 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/signup', methods=['POST'])
+def create_user():
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
+
+    if None in [email, password]:
+        return jsonify({"message": "email and password are requiered"}), 400
+
+    email_already_exists = db.session.execute(db.select(User).filter_by(email=email)).one_or_none()
+    if email_already_exists:
+        return jsonify({"message": "invalid credentials"}), 400
+    
+    new_user = User(email=email, password=password)
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify({"message": "DB error"}), 500
+    
+    return jsonify({"message":"User registered succesfully"}), 200
+
+@api.route('/login', methods=['POST'])
+def login_user():
+    data = request.json
+    email = data.get("email")
+    password = data.get("password")
+
+    if None in [email, password]:
+        return jsonify({"message": "email and password are requiered"}), 400
+    
+    user = User.query.filter_by(email=email).first()
+
+    if user is None or user.password != password:
+        return jsonify({"message": "invalid credentials"})
+    
+    token = create_access_token(identity=user.id)
+
+    return jsonify({"message": token}), 200
+
+@api.route('/user/<int:id>', methods=['get'])
+@jwt_required()
+def get_user_data(id):
+    user = User.query.get(id)
+    if user is None:
+        return jsonify({"message": "User not found"})
+
+    user_data = {
+         "id": user.id,
+        "email": user.email
+    }
+
+    return jsonify(user_data), 200
--- src/app.py
+++ src/app.py
@@ -10,6 +10,7 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import JWTManager
 
 # from models import Person
 
@@ -40,9 +41,16 @@ setup_commands(app)
 # Add all endpoints form the API with a "api" prefix
 app.register_blueprint(api, url_prefix='/api')
 
+
+
+
+# JWT CONFIG
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+jwt = JWTManager(app)
+
+
+
 # Handle/serialize errors like a JSON object
-
-
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
