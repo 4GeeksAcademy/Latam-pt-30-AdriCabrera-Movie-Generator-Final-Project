3bc6a21b2944dc9b9a3a7b468afb8fd6b1ca33a0
matischlegel <161637922+matischlegel@users.noreply.github.com>
2024-07-26 23:26:27 +0000

    saque el recomended del navbar y del layuout para que no se pueda acceder, pero deje el codigo disponible

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -27,9 +27,6 @@ export const Navbar = () => {
 							</li>
 
 						}
-						<li className="nav-item">
-							<Link to={"/recomended"} className="nav-link" href="#">Recomendaciones</Link>
-						</li>
 					</ul>
 					<form className="d-flex" role="search">
 						{store.token ? (
